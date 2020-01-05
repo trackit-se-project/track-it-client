ad362985abc5c466df7751645f335082f2df3f9a
Bogdan Georgescu <bogdan.georgescu.gbm@gmail.com>
2020-01-05 14:53:20 +0200

    implemented water tracker with quick amount add

--- components/screens/waterTracker.js
+++ components/screens/waterTracker.js
@@ -1,10 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View, Text, Button } from "react-native";
 
-export default function WaterTracker({ changeScreen }) {
+export default function WaterTracker({ user, changeScreen }) {
+  const getDate = d => {
+    const date = d ? new Date(d) : new Date();
+    return (
+      (date.getDate() > 9 ? date.getDate() : "0" + date.getDate()) +
+      "/" +
+      (date.getMonth() > 8
+        ? date.getMonth() + 1
+        : "0" + (date.getMonth() + 1)) +
+      "/" +
+      date.getFullYear()
+    );
+  };
+
+  const fetchTotalAmount = () => {
+    fetch(
+      "http://192.168.1.5:3000/water?userId=" + user._id + "&date=" + getDate()
+    )
+      .then(res => res.json())
+      .then(json => {
+        setTotalAmount(json.totalAmount);
+      })
+      .catch(err => console.log(err));
+
+    return 0;
+  };
+
+  const [totalAmount, setTotalAmount] = useState(fetchTotalAmount());
+
+  const addAmount = (amount, date = null) => {
+    fetch("http://192.168.1.5:3000/water", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        userId: user._id,
+        date: getDate(date),
+        amount: amount
+      })
+    })
+      .then(res => res.json())
+      .then(json => {
+        setTotalAmount(prevTotalAmount => prevTotalAmount + json.amount);
+      })
+      .catch(err => console.log(err));
+  };
+
+  function Separator() {
+    return <View style={styles.separator} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>WaterTracker Screen</Text>
+      <Text style={styles.banner}>WaterTracker</Text>
+      <Text style={styles.totalAmount}>{totalAmount}/2000</Text>
+      <View style={styles.quickAddGroup}>
+        <Button title="200ml" onPress={() => addAmount(200)}></Button>
+        <Button title="330ml" onPress={() => addAmount(330)}></Button>
+        <Button title="500ml" onPress={() => addAmount(500)}></Button>
+      </View>
+      <Separator />
+      <Button title="Custom add" onPress={() => changeScreen("menu")}></Button>
+      <Separator />
       <Button title="Back" onPress={() => changeScreen("menu")}></Button>
     </View>
   );
@@ -12,9 +73,21 @@ export default function WaterTracker({ changeScreen }) {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: "column",
     alignItems: "center",
-    justifyContent: "center"
+    paddingTop: 100
+  },
+  banner: {
+    fontSize: 24
+  },
+  totalAmount: {
+    fontSize: 36,
+    marginTop: 100,
+    marginBottom: 100
+  },
+  quickAddGroup: {
+    marginVertical: 0
+  },
+  separator: {
+    marginVertical: 20
   }
 });
